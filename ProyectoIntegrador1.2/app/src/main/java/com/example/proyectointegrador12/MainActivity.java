b689c5472b58df0b8af96234828c1fa07103b8cf
GODston <gastontejedav@gmail.com>
2021-03-09 21:28:40 -0600

    Creación de Proyecto
    Se crea la version 1.2

---
+++ ProyectoIntegrador1.2/app/src/main/java/com/example/proyectointegrador12/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.proyectointegrador12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        Lista de Nombres:
+        -Gaston Tejeda Villaseñor 1889295
+        -
+        -
+        -
+         */
+    }
+}
