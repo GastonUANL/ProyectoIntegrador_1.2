b959863826d91cc37bbcefaf6e9768742785113d
GODston <gastontejedav@gmail.com>
2021-04-13 20:33:15 -0500

    Vista Noticia
    Se crea la vista de Noticia

---
+++ ProyectoIntegrador1.2/app/src/main/java/com/example/proyectointegrador12/VistaNoticia.java
@@ -0,0 +1,15 @@
+package com.example.proyectointegrador12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class VistaNoticia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_noticia);
+    }
+}
